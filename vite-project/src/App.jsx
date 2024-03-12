c6a78b98da34f55e132e211fb909384063ca6059
EmmanuelCastaneda <151039652+EmmanuelCastaneda@users.noreply.github.com>
2024-03-12 10:47:27 -0500

    Tercer commit merge

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
     
     setData([
       { ID:1,name: "Juan Pedro", description: "Se fue a su casas despues de trabajar" },
-      { ID:2,name: "Maria Becerra", description: "El autotum esta fuerte" },
-      { ID:3,name: "Higuain Maradona", description: "Extramvotico" },
+      { ID:2,name: "Maria Becerra", description: "Canta bien pero no tanto para que me guste" },
+      { ID:3,name: "Higuain Maradona", description: "Inigualable como la luna" },
       { ID:4,name:"Daniela Astudillo Paz",description:"Amor a primera viasta"},
-      { ID:5,name:"Restrepo Iguana",description:"Forentse"}
+      { ID:5,name:"Restrepo Iguana",description:"Alguien quien su sacrifico fue tan grande como el amor"}
     ]);
   }, []);
 
@@ -24,11 +24,11 @@ function App() {
 
   return (
     <div className="contenedor">
-      <InputComponent filterText={filterText} handleFilterChange={handleFilterChange} />
-      <TableComponent data={data} filterText={filterText} />
+      <InputComponent filterText={filterText} handleFilterChange={handleFilterChange}></InputComponent>
+      <TableComponent data={data} filterText={filterText}></TableComponent>
+      
     </div>
   );
 };
 
-
-
+export default App;
