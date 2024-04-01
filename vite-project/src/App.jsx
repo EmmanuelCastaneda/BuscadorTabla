6c232b0f0785dd2c745794d9d85b5387309c78f6
EmmanuelCastaneda <destroyemacasta@gmail.com>
2024-04-01 12:50:08 -0500

    Segundo Commit Eliminar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import InputComponent from './components/InputComponent';
 import TableComponent from './components/TableComponent';
+import SelectComponent from './components/select';
 import React, { useState, useEffect } from "react";
 import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
   const [filterText, setFilterText] = useState("");
+  const [selectedOption, setSelectedOption] = useState('5');
 
   useEffect(() => {
     
@@ -14,21 +16,41 @@ function App() {
       { ID:2,name: "Maria Becerra", description: "Canta bien pero no tanto para que me guste" },
       { ID:3,name: "Higuain Maradona", description: "Inigualable como la luna" },
       { ID:4,name:"Daniela Astudillo Paz",description:"Amor a primera viasta"},
-      { ID:5,name:"Restrepo Iguana",description:"Alguien quien su sacrifico fue tan grande como el amor"}
+      { ID:5,name:"Restrepo Iguana",description:"Alguien quien su sacrifico fue tan grande como el amor"},
+      {ID:6,name:"Camilo Angel",description:"El mas listo del curso"},
+      {ID:7,name:"Alejando Balverde" ,description:"No se juega bien al futbol"},
+      {ID:8,name:"Rafael Valderrama",description:"Una estrella que duro poco"},
+      {ID:9,name:"Defrauncio Orgalau",description:"Queria ser Italiano pero nacio en LATAM"},
+      {ID:10,name:"Walfrer Mayonesa",description:"Un sabor que no convina tanto"},
+      {ID:11,name:"Samuel Eto",description:"Brillo en el Barcelona tanto como el una estrella"},
+      {ID:12,name:"Fernando Alonso",description:"Gambero Ilusor "},
+      {ID:13,name:"Pepa You",description:"La reyna del baile"},
+      {ID:14,name:"Fererro Rocchel",description:"Tiene un gran corazón"},
+      {ID:15,name:"Julian Arbol",description: "Es muy bueno juegando tenis"},
+      {ID:16,name:"Gafekond Ñorfer",description: "Jogo bonito Brazil"},
+      {ID:17,name:"Deftor Haora",description:"Desructor de planetas"},
+      {ID:18,name:"Fidusuario Tendariano",description: "Conoce todos los secretos"},
+      {ID:19,name:"Asimilia perez",description: "Sabe mucho de todo"},
+      {ID:20,name:"Comoder Casialgo",description: "Este es el comodor"}
     ]);
   }, []);
 
   const handleFilterChange = (event) => {
     setFilterText(event.target.value);
   };
+  const handleSelectChange = (selectedValue) => {
+    setSelectedOption(selectedValue);
+  };
 
   return (
     <div className="contenedor">
+      <SelectComponent data={data} handleSelectChange={handleSelectChange}></SelectComponent>
       <InputComponent filterText={filterText} handleFilterChange={handleFilterChange}></InputComponent>
-      <TableComponent data={data} filterText={filterText}></TableComponent>
+      <TableComponent data={data} filterText={filterText} selectedOption={selectedOption}></TableComponent>
       
     </div>
   );
-};
+}
+
 
 export default App;
--- vite-project/src/components/TableComponent.jsx
+++ vite-project/src/components/TableComponent.jsx
@@ -1,7 +1,8 @@
 import './TableComponent.css'
 
-function TableComponent({ data, filterText }) {
+function TableComponent({ data, filterText,selectedOption }) {
   const filteredData = data.filter(item => item.name.toLowerCase().includes(filterText.toLowerCase()));
+  const filteredData2 = data.slice(0, parseInt(selectedOption));
 
   return (
     <table className="data-table">
@@ -10,16 +11,34 @@ function TableComponent({ data, filterText }) {
             <th>ID</th>
             <th>NOMBRE</th>
             <th>Descripción</th>
+            <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
-        {filteredData.map((item) => (
+        {filteredData.map((item)=>(
           <tr key={item.name}>
             <td>{item.ID}</td>
             <td>{item.name}</td>
             <td>{item.description}</td>
+            <td>
+              <button className="btn btn-primary" onClick={()=>handleClick(item)}>Editar</button>
+              <button className="btn btn-danger" onClick={()=>handleDelete(item)}>Borrar</button>
+            </td>
           </tr>
         ))}
+        {filteredData2.map((item) =>(
+          <tr key={item.name}>
+          <td>{item.ID}</td>
+          <td>{item.name}</td>
+          <td>{item.description}</td>
+          <td>
+            <button className="btn btn-primary" onClick={() => editar(item)}>Editar</button>
+            <button className="btn btn-danger" onClick={() => borrar(item)}>Borrar</button>
+          </td>
+        </tr>
+     
+        ))}
+      
       </tbody>
     </table>
   );
